612017a2be85069014e6805735db2eca3b4255c6
imygnam <imygnam@gmail.com>
2023-06-18 20:00:50 +0900

    [FIX] uri

--- src/service/quizService.js
+++ src/service/quizService.js
@@ -8,7 +8,7 @@ class QuizService {
     }
 
     async getNNList(input){
-        let url = "http://localhost:8080/parse?input=" + input;
+        let url = "http://prod-iterview-spring-boot-service/parse?input=" + input;
         let response = await fetch(url);
         let data = await response.json();
         return data;
@@ -161,7 +161,7 @@ class QuizService {
         }
     }
 
-    async reportQuiz(quizId, userId, report){
+    async reportQuiz(quizListId, userId, answer) {
         return {
             result: true,
             message: "개발중 입니다"
